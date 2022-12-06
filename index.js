ba826a53a81e61b9b63f487c00b1d2d18f380747
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-05 20:06:44 -0800

    commit tree 7e1608385dc80f0ccdb24bcc09d1cc13f4a45bea

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arrayOfStringNames, eventName) {
+    let card = [];
+    for(let i = 0; i < arrayOfStringNames.length; i++){
+        card.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return card;
+}
+
+function countDown(positiveInteger) {
+    while (positiveInteger >=0) {
+        console.log(positiveInteger);
+        positiveInteger -= 1;
+    }
+}
